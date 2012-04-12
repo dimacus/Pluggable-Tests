768ab6c7d3842033016aebdf430e6158a6df28f9
Dima Kovalenko <dima@groupon.com>
2012-04-12 15:02:20 -0400

    Adding steps for selenium tests

---
+++ features/step_defenitions/selenium/deals_steps.rb
@@ -0,0 +1,19 @@
+require 'rubygems'
+require 'selenium-webdriver'
+require 'hpricot'
+
+Given /^I am viewing all deals for San Francisco$/ do
+  @driver = Selenium::WebDriver.for :firefox
+end
+
+Then /^I should see a list of deals related to San Francisco$/ do
+  @driver.navigate.to "http://www.groupon.com/san-francisco/all"
+
+  @driver.title.should == "All San Francisco Deals | Groupon"
+
+  page_source = Hpricot(@driver.page_source)
+  page_source.search("div.deal").size.should > 0
+  page_source.at("h2#new_drawer_anchor a").inner_text.should == "San Francisco"
+
+  @driver.close
+end
